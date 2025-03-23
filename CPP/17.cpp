5d47ad5e5bc6a559f215e7267e94fe9f90fa0dee
MrKeanu <warmthwind@163.com>
2025-03-23 20:38:54 +0800

    Daily Code:Letter Combinations of a Phone Number [Medium - 4 - backtrack]

---
+++ CPP/17.cpp
@@ -0,0 +1,40 @@
+/*
+17. Letter Combinations of a Phone Number [Medium - 4]
+*/
+#include "pub_template.h"
+using namespace pub;
+
+class Solution {
+public:
+    unordered_map<char, string> mp = {
+        {'2', "abc"}, {'3', "def"}, {'4', "ghi"}, {'5', "jkl"}, {'6', "mno"},{'7', "pqrs"}, {'8', "tuv"}, {'9', "wxyz"}
+    };
+    // 解法一 回溯 选或不选 Time: O(4^n) Space: O(n)
+    vector<string> letterCombinations(string digits) {
+        int n = digits.length();
+        if (n == 0) return {};
+        vector<string> res;
+        string path;
+        function<void(int)> dfs = [&](int idx)->void {
+            if (idx == n) return res.push_back(path);
+            for (char c : mp[digits[idx]]) {
+                path.push_back(c);
+                dfs(idx + 1);
+                path.pop_back();
+            }
+        };
+        dfs(0);
+        return res;
+    }
+
+    void test() {
+        string digits = "234";
+        vector<string> res = letterCombinations(digits);
+        print1DVector(res);
+    }
+};
+
+//int main() {
+//    Solution().test();
+//    return 0;
+//}
