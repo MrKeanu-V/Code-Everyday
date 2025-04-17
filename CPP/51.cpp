02e5f9a47f16219091e5d8945a01c3d5a890068b
MrKeanu <warmthwind@163.com>
2025-04-17 14:08:50 +0800

    N-Queens

--- CPP/51.cpp
+++ CPP/51.cpp
@@ -31,6 +31,30 @@ public:
         dfs(0);
         return res;
     }
+    // 解法二 回溯+数组
+    vector<vector<string>> solveNQueens_2(int n) {
+        vector<vector<string>> res;
+        vector<int> queens(n);
+        vector<int> cols(0), diag1(n * 2 - 1), diag2(n * 2 - 1);
+        function<void(int)> dfs = [&](int r) {
+            if (r == n) {
+                vector<string> board(n);
+                for (int i = 0; i < n; i++) board[i] = string(queens[i], '.') + 'Q' + string(n - queens[i] - 1, '.');
+                return res.push_back(board);
+            }
+            for (int c = 0; c < n; c++) {
+                int rc = r - c + n - 1;
+                if (!cols[c] && !diag1[r + c] && !diag2[rc]) {
+                    queens[r] = c;
+                    cols[c] = diag1[r + c] = diag2[rc] = true;
+                    dfs(r + 1);
+                    cols[c] = diag1[r + c] = diag2[rc] = false;
+                }
+            }
+        };
+        dfs(0);
+        return res;
+    }
 
     void test() {
         vector<vector<string>> res = solveNQueens(4);
