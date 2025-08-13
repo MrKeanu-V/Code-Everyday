bc3e29556079fc2fd0d80418ce33f64d650186c7
MrKeanu <warmthwind@163.com>
2025-08-13 20:30:38 +0800

    230. Kth Smallest Element in a BST [Medium - 3 - BST]

---
+++ CPP/230.cpp
@@ -0,0 +1,33 @@
+/*
+230. Kth Smallest Element in a BST [Medium - 3]
+*/
+#include "pub_template.h"
+using namespace pub;
+
+class Solution {
+private:
+    void inorder(TreeNode* node, vector<int>& list) {
+        if (node == nullptr) return;
+        list.emplace_back(node->val);
+        inorder(node->left, list);
+        inorder(node->right, list);
+    }
+public:
+    // Solution 1: Inorder Traversal + Sort Time: O(nlogn) Space: O(n)
+    int kthSmallest(TreeNode* root, int k) {
+        vector<int> list;
+        inorder(root, list);
+        sort(list.begin(), list.end());
+        return list[k - 1];
+    }
+    void test() {
+        vector<int> nums = { 3,1,4,NULL,2};
+        TreeNode* root = createTree(nums);
+        cout << kthSmallest(root, 1) << endl;
+    }
+};
+
+int main() {
+    Solution().test();
+    return 0;
+}
